c68f55f20d265b80a5be9d3c546ff74d3df26f05
Sarwaan <Sarwaan.Ansari@Disney.com>
2022-10-27 08:39:12 -0400

    console log output

--- code/web-api/country-prediction/src/app.ts
+++ code/web-api/country-prediction/src/app.ts
@@ -63,7 +63,11 @@ export const getDalleImageHandler = async (event: APIGatewayEvent , context: Con
     var messageBody = {};
     var statusCode = 502;
 
+    console.log(response ?? "{}")
+
     if (response?.isComplete?.BOOL === true) {
+        const link = response?.link?.S ?? 'Should be a string'
+
         statusCode = 200;
         messageBody = {
             link: response?.link?.S ?? ''
